06c0a2006c9f07eccca2cdbc70728e4a1725c115
ctesene <info@christesene.com>
2017-02-10 10:19:52 -0800

    dynamic status badges

--- routes/projects.js
+++ routes/projects.js
@@ -6,6 +6,8 @@ var _ = require("lodash");
 /* GET projects listing. */
 router.get('/', function(req, res) {
   let viewModel = projects.projects.map((project) => {
+    project.currentBuildStatusCssClass = project.currentBuildStatus === "Passing" ? "success" : "danger";
+    project.currentPullRequestCssClass = project.openPullRequest > 5 ? "warning" : "success";
     return project;
   });
 
