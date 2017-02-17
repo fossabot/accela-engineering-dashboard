abcdd29fc37ad5c5103984199a64e30b18df8693
ctesene <info@christesene.com>
2017-02-17 14:34:51 -0800

    use mock service if they don't have proper config

--- services/vsts-service.js
+++ services/vsts-service.js
@@ -2,4 +2,10 @@ var liveVstsService = require("./live/vsts-service");
 var mockVstsService = require("./mocks/vsts-service");
 var config = require("../config/app.json");
 
-module.exports = config.useMocks ? mockVstsService : liveVstsService;
+let hasProperConfig = !!process.env.VSTS_URI && !!process.env.VSTS_KEY;
+
+if (!hasProperConfig) {
+  console.log("You need the proper VSTS keys. Using mock data for now");
+}
+
+module.exports = config.useMocks || !hasProperConfig ? mockVstsService : liveVstsService;
