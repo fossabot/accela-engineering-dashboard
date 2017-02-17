7e2f12b690481711bd363086dce0ebaafd213619
ctesene <info@christesene.com>
2017-02-16 17:52:23 -0800

    home and projects HTML files added in angular

--- app/pages/projects/projects.ts
+++ app/pages/projects/projects.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: '/pages/projects/projects.html'
 })
 export class ProjectsPage {
+  projects: any[];
+
+  ngOnInit() {
+    this.projects = [
+      { id: 1, name: "PMA", description: "Productivity Mobile App", jenkinsLatestBuildStatus: "good", gitHubPullRequestsCount: 3 },
+      { id: 2, name: "ACA", description: "Accela Citizen Access", jenkinsLatestBuildStatus: "failed", gitHubPullRequestsCount: 4 },
+      { id: 3, name: "AA", description: "Accela Automation", jenkinsLatestBuildStatus: "good", gitHubPullRequestsCount: 5 },
+      { id: 4, name: "AGIS", description: "Accela GIS", jenkinsLatestBuildStatus: "good", gitHubPullRequestsCount: 6 },
+    ];
+  }
 }
